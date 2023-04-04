132bfc6d72b9ab6ed2cc90f16fd94f84e7dfd518
Quentin <qpompliano@gmail.com>
2023-04-04 13:00:55 -0400

    added progress section and progress bar

---
+++ src/lib/components/progress/ProgressBar.svelte
@@ -0,0 +1,24 @@
+<script>
+	/**
+   * @type {string}
+   */
+	 export let title;
+	/**
+   * @type {number}
+   */
+	 export let percentage;
+	$: completeClass = title === 'Completed' ? 'bg-green-four' : 'bg-leaf-one';
+</script>
+
+<div>
+	<div class="flex justify-between text-base font-normal text-gray-three">
+		<p>{title}</p>
+		<p>{percentage}%</p>
+	</div>
+	<div class="mt-5 h-4 w-full overflow-hidden rounded-full bg-gray-one">
+		<div
+			class="{completeClass} h-4 rounded-full transition-all duration-500 ease-out"
+			style="width: {percentage}%"
+		/>
+	</div>
+</div>
