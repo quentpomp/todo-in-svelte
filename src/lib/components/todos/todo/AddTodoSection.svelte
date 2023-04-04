2cc988d8a681fbbdc1950143261d15d2950ebe3d
Quentin <qpompliano@gmail.com>
2023-04-04 13:06:20 -0400

    changed styling slightly

--- src/lib/components/todos/todo/AddTodoSection.svelte
+++ src/lib/components/todos/todo/AddTodoSection.svelte
@@ -6,7 +6,7 @@
 
 <section class="flex justify-between">
   <h3 class="text-xl font-bold text-gray-three">Checklist</h3>
-  <button class="group flex items-center rounded-md border-cream-four bg-green-one px-6 py-3 text-large font-semibold text-gray-five outline outline-2 outline-offset-2 outline-green-one hover:text-green-five focus-visible:text-green-five focus-visible:outline-green-five " type="button" on:click={addTodo}>
+  <button class="group flex items-center rounded-md border-cream-four bg-green-one px-6 py-3 text-large font-semibold text-gray-five 2  hover:text-green-five focus-visible:text-green-five" type="button" on:click={addTodo}>
     Add Task
     <svg
       class="ml-3 h-4 w-4 text-gray-five group-hover:text-green-five group-focus-visible:text-green-five"
